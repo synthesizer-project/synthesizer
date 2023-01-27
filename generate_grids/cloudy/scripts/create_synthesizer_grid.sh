51f2725260db63f78a3a160732f26c842bd24b00
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-01-27 10:09:19 +0000

    updated scripts

--- generate_grids/cloudy/scripts/create_synthesizer_grid.sh
+++ generate_grids/cloudy/scripts/create_synthesizer_grid.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/"
-machine="apollo"
-grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy"
-
+# synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/" # SW's machine
+synthesizer_dir="/research/astrodata/highz/synthesizer/grids" # apollo
+# grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy"
+# grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy"
+grid="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.7_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.0_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.35_cloudy bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7_cloudy bpass-2.2.1-sin_chabrier03-0.1,100.0 bpass-2.2.1-sin_chabrier03-0.1,300.0_cloudy"
 cd ..
 python3 convert_cloudy_grid_to_hdf5.py -grid $grid -dir $synthesizer_dir
