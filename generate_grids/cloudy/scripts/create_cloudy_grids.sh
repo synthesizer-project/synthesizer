1b76858db823b559449e99514007dcc87ad16b7d
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-01 16:13:49 +0000

    updated fsps

--- generate_grids/cloudy/scripts/create_cloudy_grids.sh
+++ generate_grids/cloudy/scripts/create_cloudy_grids.sh
@@ -3,7 +3,7 @@
 synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
 machine="apollo"
 # sps="bpass-2.2.1-bin_chabrier03-0.1,100.0 "
-sps="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.0 bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.35 bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.7 bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.0 bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.35 bpass-2.2.1-bin_chabrier03-0.1,100.0 bpass-2.2.1-bin_chabrier03-0.1,300.0 bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.0 bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35 bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7 bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0 bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35 bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7 bpass-2.2.1-sin_chabrier03-0.1,100.0 bpass-2.2.1-sin_chabrier03-0.1,300.0"
+# sps="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.0 bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.35 bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.7 bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.0 bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.35 bpass-2.2.1-bin_chabrier03-0.1,100.0 bpass-2.2.1-bin_chabrier03-0.1,300.0 bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.0 bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35 bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7 bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0 bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35 bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7 bpass-2.2.1-sin_chabrier03-0.1,100.0 bpass-2.2.1-sin_chabrier03-0.1,300.0"
 p="default_param.yaml"
 c=$CLOUDY17
 
