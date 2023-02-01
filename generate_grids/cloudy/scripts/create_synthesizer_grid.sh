a95c89908fa0c4c3ca318202dda11f321bb372ab
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-01 16:45:29 +0000

    updated fsps

--- generate_grids/cloudy/scripts/create_synthesizer_grid.sh
+++ generate_grids/cloudy/scripts/create_synthesizer_grid.sh
@@ -3,8 +3,8 @@
 synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
 # grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy"
 # grid="bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy"
-grid="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.7_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.0_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.35_cloudy bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7_cloudy bpass-2.2.1-sin_chabrier03-0.1,100.0 bpass-2.2.1-sin_chabrier03-0.1,300.0_cloudy"
-# grid="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy" # failed
-grid="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy" # done
+# grid="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy" # done
+grid="bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,100.0-1.3,2.7_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.0_cloudy bpass-2.2.1-bin_bpl-0.1,1.0,300.0-1.3,2.35_cloudy bpass-2.2.1-bin_chabrier03-0.1,100.0_cloudy bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35_cloudy bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7_cloudy bpass-2.2.1-sin_chabrier03-0.1,100.0 bpass-2.2.1-sin_chabrier03-0.1,300.0_cloudy"
+
 cd ..
 python3 convert_cloudy_grid_to_hdf5.py -grid $grid -dir $synthesizer_dir
