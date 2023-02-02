b9cf84a9e3d0f2371c36460e57a538cf98ed4e9c
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-02 11:06:16 +0000

    updated scripts

--- generate_grids/cloudy/scripts/create_synthesizer_grid.sh
+++ generate_grids/cloudy/scripts/create_synthesizer_grid.sh
@@ -13,10 +13,10 @@ grid=""
 # grid=$sps" bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7_cloudy bpass-2.2.1-sin_chabrier03-0.1,100.0_cloudy bpass-2.2.1-sin_chabrier03-0.1,300.0_cloudy"
 
 # bpass alpha variation
-grid=$sps" bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpham0.2 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.0 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.2 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.4 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.6 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.8 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha1.0"
+grid=$grid" bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpham0.2 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.0 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.2 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.4 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.6 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha0.8 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-alpha1.0"
 
 # bpass U variation
-grid=$sps" bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm1 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm2 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm3 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm4"
+# grid=$grid" bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm1 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm2 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm3 bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-log10U_refm4"
 
 # # FSPS slope variants
 # sps=$sps" fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,1.5_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,1.6_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,1.7_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,1.8_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,1.9_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.0_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.2_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.3_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.4_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.5_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.6_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.7_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.8_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,2.9_cloudy fsps-3.2_bpl-0.08,0.5,1,120-1.3,2.3,3.0_cloudy"
