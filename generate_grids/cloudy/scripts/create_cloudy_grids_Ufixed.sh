17d1541cccb8949d52a47c05422c4f5aea01ce76
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-13 13:22:46 +0000

    bugfix

---
+++ generate_grids/cloudy/scripts/create_cloudy_grids_Ufixed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/"
+synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
+machine="apollo"
+sps="bpass-2.2.1-bin_chabrier03-0.1,300.0" # FLARE default
+p="vary_Ufixed.yaml"
+c=$CLOUDY17
+
+cd ..
+python3 make_cloudy_input_grid_array.py -dir $synthesizer_dir -m $machine -sps $sps  -p $p  -c $c
