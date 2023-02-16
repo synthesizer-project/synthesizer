d48591fb335facbc2344e11081f19bd7b59dcde4
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-02-16 19:32:07 +0000

    updated

---
+++ generate_grids/cloudy/scripts/create_cloudy_grids_vary_fixedU.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# create a cloudy grid using the default assumptions + parameter variation
+
+# synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/"
+synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
+machine="apollo"
+
+sps="bpass-2.2.1-bin_chabrier03-0.1,300.0 "
+
+p="vary_fixedU.yaml"
+c=$CLOUDY17
+
+cd ..
+python3 make_cloudy_input_grid.py -dir $synthesizer_dir -m $machine -sps $sps  -p $p  -c $c
