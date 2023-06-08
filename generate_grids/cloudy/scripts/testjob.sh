017a286e28706f377083c0f54573b2a0502171e0
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-08 08:28:41 +0100

    removed scripts to a separate repo (synthesizer_grid_creation_scripts

--- generate_grids/cloudy/scripts/testjob.sh
+++
@@ -1,8 +0,0 @@
-
-
-index=5
-
-# access line at index from input_names file
-grid=$(sed "${index}q;d" grids.txt)
-
-echo $grid
--- generate_grids/cloudy/scripts/testread.sh
+++
@@ -1,9 +0,0 @@
-let index=2
-
-# access line at index from input_names file
-p=$(sed "${index}q;d" spsparams.dat)
-arrIN=(${p// / })
-sps=${arrIN[0]}
-params=${arrIN[1]}
-echo $sps
-echo $params
