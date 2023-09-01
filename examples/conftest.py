3cbf6cf7c8d33a2449a44283da6cc010d595bbe1
christopherlovell <chrisl1991@hotmail.co.uk>
2023-09-01 10:57:49 +0100

    conftest test

---
+++ examples/conftest.py
@@ -0,0 +1,20 @@
+def pytest_collectreport(report):
+    print("CONFTEST loaded")
+
+def pytest_configure(config):
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    import matplotlib as mpl
+    mpl.use('template')
+
+# def pytest_sessionstart(session):
+#     """
+#     Called after the Session object has been created and
+#     before performing collection and entering the run test loop.
+#     """
+#     import matplotlib as mpl
+#     mpl.use('template')
+    
