cc56420472bd84fd8887f3402714804e60d84ed1
christopherlovell <chrisl1991@hotmail.co.uk>
2023-09-01 11:48:34 +0100

    add to tests dir

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+# conftest.py
+import pytest
+
+def pytest_collectreport(report):
+    print("CONFTEST loaded")
+
+@pytest.fixture
+def currpath(request):
+    return str(request.node.fspath)
+
+def pytest_configure():
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    import matplotlib as mpl
+    mpl.use('template')
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and
+    before performing collection and entering the run test loop.
+    """
+    import matplotlib as mpl
+    mpl.use('template')
