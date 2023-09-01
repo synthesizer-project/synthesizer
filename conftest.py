f48d9ac966a481914cb75ef8479a5706285d4924
Chris Lovell <chrisl1991@hotmail.co.uk>
2023-09-01 09:47:07 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,16 @@
-def pytest_configure():
-    """ called before ``pytest_runtest_call(item). """
+# def pytest_configure():
+#     """
+#     Allows plugins and conftest files to perform initial configuration.
+#     This hook is called for every plugin and initial conftest
+#     file after command line options have been parsed.
+#     """
+#     import matplotlib as mpl
+#     mpl.use('template')
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and
+    before performing collection and entering the run test loop.
+    """
     import matplotlib as mpl
-    mpl.use('template')  
+    mpl.use('template')
