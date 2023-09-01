5db50cc98afda337d075e92c4a6309696143d3aa
Chris Lovell <chrisl1991@hotmail.co.uk>
2023-09-01 10:15:35 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
-# def pytest_configure():
-#     """
-#     Allows plugins and conftest files to perform initial configuration.
-#     This hook is called for every plugin and initial conftest
-#     file after command line options have been parsed.
-#     """
-#     import matplotlib as mpl
-#     mpl.use('template')
+# conftest.py
+import pytest
+
+@pytest.fixture
+def currpath(request):
+    return str(request.node.fspath)
+
+def pytest_configure():
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    import matplotlib as mpl
+    mpl.use('template')
 
 def pytest_sessionstart(session):
     """
