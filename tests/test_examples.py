7ede4eeafcb823d06bdc9c9e1d7ab26d78988e25
christopherlovell <chrisl1991@hotmail.co.uk>
2023-09-01 12:49:09 +0100

    test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,12 +7,9 @@ scripts = pathlib.Path(__file__).parents[1]
 scripts = scripts / "examples"
 scripts = scripts.rglob("*.py")
 
-# for p in scripts.rglob("*.py"):
-#     print(p)
-
-# for path in scripts:
-#     print(path)
 
 @pytest.mark.parametrize('script', scripts)
 def test_script_execution(script):
+    import matplotlib as mpl
+    mpl.use('template')
     runpy.run_path(str(script))
