91bfbe3f893a0de1e842b209995a788a49f7a642
wjr21 <wjr21@sussex.ac.uk>
2024-04-03 15:04:18 +0100

    Typed exceptions

--- src/synthesizer/exceptions.py
+++ src/synthesizer/exceptions.py
@@ -38,427 +38,314 @@ Exceptions:
                as particles not lying within a grid, missing axes etc.
 """
 
+from typing import Optional
+
 
 class MissingArgument(Exception):
     """Generic exception class for when an argument is missing."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the MissingArgument class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Missing argument"
+        return self.message if self.message else "Missing argument"
 
 
 class IncorrectUnits(Exception):
     """Generic exception class for when incorrect units are provided."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the IncorrectUnits class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Inconsistent units"
+        return self.message if self.message else "Incorrect units"
 
 
 class MissingUnits(Exception):
     """Generic exception class for when expected units aren't provided."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the MissingUnits class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Units are missing"
+        return self.message if self.message else "Units are missing"
 
 
 class InconsistentParameter(Exception):
     """Generic exception class for inconsistent parameters."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the InconsistentParameter class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Inconsistent parameter choice"
+        return (
+            self.message if self.message else "Inconsistent parameter choice"
+        )
 
 
 class InconsistentArguments(Exception):
     """Generic exception class for inconsistent combinations of arguments."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the InconsistentArguments class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Inconsistent parameter choice"
+        return self.message if self.message else "Inconsistent arguments"
 
 
 class UnimplementedFunctionality(Exception):
     """Generic exception class for functionality not yet implemented."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the UnimplementedFunctionality class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Unimplemented functionality!"
+        return self.message if self.message else "Unimplemented functionality"
 
 
 class UnknownImageType(Exception):
-    """Generic exception class for functionality not yet implemented."""
+    """Generic exception class for unknown image types."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the UnknownImageType class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            return "Inconsistent parameter choice"
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Inconsistent units"
+        return self.message if self.message else "Unknown image type"
 
 
 class InconsistentAddition(Exception):
     """Generic exception class for when adding two objects is impossible."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the InconsistentAddition class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "Unable to add"
+        return self.message if self.message else "Inconsistent addition"
 
 
 class InconsistentCoordinates(Exception):
     """Generic exception class for when coordinates are inconsistent."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the InconsistentCoordinates class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "Coordinates are inconsistent"
+        return self.message if self.message else "Inconsistent coordinates"
 
 
 class SVOFilterNotFound(Exception):
     """Exception class for when an SVO filter code is not in the database."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the SVOFilterNotFound class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "Filter not found!"
+        return self.message if self.message else "SVO filter not found"
 
 
 class InconsistentWavelengths(Exception):
-    """Exception class for when array dimensions don't."""
+    """Exception class for when array dimensions don't match."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the InconsistentWavelengths class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "Coordinates are inconsistent"
+        return self.message if self.message else "Inconsistent wavelengths"
 
 
 class MissingSpectraType(Exception):
-    """Exception class for when an SPS grid is missing."""
+    """Exception class for when an SPS grid is missing a spectra type."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the MissingSpectraType class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "Spectra type not in grid!"
+        return self.message if self.message else "Missing spectra type"
 
 
 class MissingImage(Exception):
     """Exception class for when an image has not yet been made."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the MissingImage class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "Image not yet created!"
+        return self.message if self.message else "Missing image"
 
 
 class WavelengthOutOfRange(Exception):
-    """Exception class for when a wavelength is outside a Filter."""
+    """Exception class for when a wavelength is outside a Filter's range."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the WavelengthOutOfRange class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        else:
-            return "The provided wavelength is out of the filter range!"
+        return self.message if self.message else "Wavelength out of range"
 
 
 class SVOInaccessible(Exception):
     """Generic exception class for when SVO is inaccessible."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the SVOInaccessible class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "SVO database is down!"
+        return self.message if self.message else "SVO database is down"
 
 
 class UnrecognisedOption(Exception):
-    """Generic exception class for when an argument is not recognised."""
+    """Generic exception class for when a string argument is not recognised."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the UnrecognisedOption class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Unrecognised option."
+        return self.message if self.message else "Unrecognised option"
 
 
 class MissingAttribute(Exception):
     """Generic exception class for when a required attribute is missing."""
 
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the MissingAttribute class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Missing attribute"
+        return self.message if self.message else "Missing attribute"
 
 
 class GridError(Exception):
-    """
-    Generic exception class for anything to with grid issues.
+    """Generic exception class for issues related to grids."""
 
-    e.g. particles not lying within a grid, missing axes etc.
-    """
-
-    def __init__(self, *args):
+    def __init__(self, message: Optional[str] = None) -> None:
         """
         Initialise the GridError class.
 
         Args:
-            *args: A list of arguments. The first argument is the message to
-                   display.
+            message: The message to display.
         """
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+        self.message = message
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return the message to display."""
-        if self.message:
-            return "{0} ".format(self.message)
-        return "Theres an issues with the grid."
+        return (
+            self.message if self.message else "There's an issue with the grid"
+        )
