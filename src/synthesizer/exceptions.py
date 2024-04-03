9e6dbd78a355841367d5f08df88c22526d320c35
wjr21 <wjr21@sussex.ac.uk>
2024-04-03 14:57:41 +0100

    Fully documented exceptions

--- src/synthesizer/exceptions.py
+++ src/synthesizer/exceptions.py
@@ -1,134 +1,223 @@
-"""
-The definitions for Synthesizer specific errors.
+"""A module for Synthesizer specific errors.
+
+This module contains the definitions for the exceptions that are raised
+by the Synthesizer package.
+
+Exceptions:
+    MissingArgument: A generic exception class for when an argument is missing.
+    IncorrectUnits: A generic exception class for when incorrect units are
+                    provided.
+    MissingUnits: A generic exception class for when expected units aren't
+                  provided.
+    InconsistentParameter: A generic exception class for inconsistent
+                           parameters.
+    InconsistentArguments: A generic exception class for inconsistent
+                           combinations of arguments.
+    UnimplementedFunctionality: A generic exception class for functionality not
+                                yet implemented.
+    UnknownImageType: A generic exception class for functionality not yet
+                      implemented.
+    InconsistentAddition: A generic exception class for when adding two objects
+                          is impossible.
+    InconsistentCoordinates: A generic exception class for when coordinates are
+                             inconsistent.
+    SVOFilterNotFound: An exception class for when an SVO filter code does not
+                       match one in the database.
+    InconsistentWavelengths: An exception class for when array dimensions don't
+                             match.
+    MissingSpectraType: An exception class for when an SPS grid is missing.
+    MissingImage: An exception class for when an image has not yet been made.
+    WavelengthOutOfRange: An exception class for when a wavelength is not
+                          accessible to Filters in a FilterCollection.
+    SVOInaccessible: A generic exception class for when SVO is inaccessible.
+    UnrecognisedOption: A generic exception class for when a string argument is
+                        not a recognised option.
+    MissingAttribute: A generic exception class for when a required attribute
+                      is missing on an object.
+    GridError: A generic exception class for anything to with grid issues, such
+               as particles not lying within a grid, missing axes etc.
 """
 
 
 class MissingArgument(Exception):
-    """
-    Generic exception class for when an argument is missing.
-    """
+    """Generic exception class for when an argument is missing."""
 
     def __init__(self, *args):
+        """
+        Initialise the MissingArgument class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Missing argument"
 
 
 class IncorrectUnits(Exception):
-    """
-    Generic exception class for when incorrect units are provided.
-    """
+    """Generic exception class for when incorrect units are provided."""
 
     def __init__(self, *args):
+        """
+        Initialise the IncorrectUnits class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Inconsistent units"
 
 
 class MissingUnits(Exception):
-    """
-    Generic exception class for when expected units aren't provided.
-    """
+    """Generic exception class for when expected units aren't provided."""
 
     def __init__(self, *args):
+        """
+        Initialise the MissingUnits class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Units are missing"
 
 
 class InconsistentParameter(Exception):
-    """
-    Generic exception class for inconsistent parameters.
-    """
+    """Generic exception class for inconsistent parameters."""
 
     def __init__(self, *args):
+        """
+        Initialise the InconsistentParameter class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Inconsistent parameter choice"
 
 
 class InconsistentArguments(Exception):
-    """
-    Generic exception class for inconsistent combinations of arguments.
-    """
+    """Generic exception class for inconsistent combinations of arguments."""
 
     def __init__(self, *args):
+        """
+        Initialise the InconsistentArguments class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Inconsistent parameter choice"
 
 
 class UnimplementedFunctionality(Exception):
-    """
-    Generic exception class for functionality not yet implemented.
-    """
+    """Generic exception class for functionality not yet implemented."""
 
     def __init__(self, *args):
+        """
+        Initialise the UnimplementedFunctionality class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Unimplemented functionality!"
 
 
 class UnknownImageType(Exception):
-    """
-    Generic exception class for functionality not yet implemented.
-    """
+    """Generic exception class for functionality not yet implemented."""
 
     def __init__(self, *args):
+        """
+        Initialise the UnknownImageType class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             return "Inconsistent parameter choice"
 
+    def __str__(self):
+        """Return the message to display."""
+        if self.message:
+            return "{0} ".format(self.message)
+        return "Inconsistent units"
+
 
 class InconsistentAddition(Exception):
-    """
-    Generic exception class for when adding two objects is impossible.
-    """
+    """Generic exception class for when adding two objects is impossible."""
 
     def __init__(self, *args):
+        """
+        Initialise the InconsistentAddition class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -136,17 +225,23 @@ class InconsistentAddition(Exception):
 
 
 class InconsistentCoordinates(Exception):
-    """
-    Generic exception class for when coordinates are inconsistent.
-    """
+    """Generic exception class for when coordinates are inconsistent."""
 
     def __init__(self, *args):
+        """
+        Initialise the InconsistentCoordinates class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -154,18 +249,23 @@ class InconsistentCoordinates(Exception):
 
 
 class SVOFilterNotFound(Exception):
-    """
-    Exception class for when an SVO filter code does not match one in
-    the database.
-    """
+    """Exception class for when an SVO filter code is not in the database."""
 
     def __init__(self, *args):
+        """
+        Initialise the SVOFilterNotFound class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -173,17 +273,23 @@ class SVOFilterNotFound(Exception):
 
 
 class InconsistentWavelengths(Exception):
-    """
-    Exception class for when array dimensions don't
-    """
+    """Exception class for when array dimensions don't."""
 
     def __init__(self, *args):
+        """
+        Initialise the InconsistentWavelengths class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -191,17 +297,23 @@ class InconsistentWavelengths(Exception):
 
 
 class MissingSpectraType(Exception):
-    """
-    Exception class for when an SPS grid is missing
-    """
+    """Exception class for when an SPS grid is missing."""
 
     def __init__(self, *args):
+        """
+        Initialise the MissingSpectraType class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -209,17 +321,23 @@ class MissingSpectraType(Exception):
 
 
 class MissingImage(Exception):
-    """
-    Exception class for when an image has not yet been made
-    """
+    """Exception class for when an image has not yet been made."""
 
     def __init__(self, *args):
+        """
+        Initialise the MissingImage class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -227,18 +345,23 @@ class MissingImage(Exception):
 
 
 class WavelengthOutOfRange(Exception):
-    """
-    Exception class for when a wavelength is not accessible to
-    Filters in a FilterCollection.
-    """
+    """Exception class for when a wavelength is outside a Filter."""
 
     def __init__(self, *args):
+        """
+        Initialise the WavelengthOutOfRange class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         else:
@@ -246,53 +369,69 @@ class WavelengthOutOfRange(Exception):
 
 
 class SVOInaccessible(Exception):
-    """
-    Generic exception class for when SVO is inaccessible.
-    """
+    """Generic exception class for when SVO is inaccessible."""
 
     def __init__(self, *args):
+        """
+        Initialise the SVOInaccessible class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "SVO database is down!"
 
 
 class UnrecognisedOption(Exception):
-    """
-    Generic exception class for when a string argument is not a recognised
-    option.
-    """
+    """Generic exception class for when an argument is not recognised."""
 
     def __init__(self, *args):
+        """
+        Initialise the UnrecognisedOption class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Unrecognised option."
 
 
 class MissingAttribute(Exception):
-    """
-    Generic exception class for when a required attribute is missing on an
-    object.
-    """
+    """Generic exception class for when a required attribute is missing."""
 
     def __init__(self, *args):
+        """
+        Initialise the MissingAttribute class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Missing attribute"
@@ -300,17 +439,26 @@ class MissingAttribute(Exception):
 
 class GridError(Exception):
     """
-    Generic exception class for anything to with grid issues, such as particles
-    not lying within a grid, missing axes etc.
+    Generic exception class for anything to with grid issues.
+
+    e.g. particles not lying within a grid, missing axes etc.
     """
 
     def __init__(self, *args):
+        """
+        Initialise the GridError class.
+
+        Args:
+            *args: A list of arguments. The first argument is the message to
+                   display.
+        """
         if args:
             self.message = args[0]
         else:
             self.message = None
 
     def __str__(self):
+        """Return the message to display."""
         if self.message:
             return "{0} ".format(self.message)
         return "Theres an issues with the grid."
