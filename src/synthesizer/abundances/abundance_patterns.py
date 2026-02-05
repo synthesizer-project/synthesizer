300b7559f943b5b726e560835d30ee693c461f59
Aswin P Vijayan <aswinpvijayan@github.com>
2026-02-05 10:25:19 +0000

    instantitate depletion model if passed as string

--- src/synthesizer/abundances/abundance_patterns.py
+++ src/synthesizer/abundances/abundance_patterns.py
@@ -20,6 +20,7 @@ import numpy as np
 from synthesizer import exceptions
 from synthesizer.abundances import (
     abundance_scalings,
+    depletion_models,
     elements,
     reference_abundance_patterns,
 )
@@ -55,6 +56,8 @@ class Abundances:
             The a synthesizer.depletion_models object or dictionary of
             depletion. Optional initialisation argument. Defaults to
             None.
+        depletion_scale (float):
+            A scaling factor to apply to the depletion pattern.
         helium_mass_fraction (float):
             The helium mass fraction (more commonly denoted as "Y").
         hydrogen_mass_fraction (float):
@@ -84,6 +87,7 @@ class Abundances:
         reference=reference_abundance_patterns.GalacticConcordance,
         depletion_pattern=None,
         depletion_model=None,
+        depletion_scale=None,
     ):
         """Initialise an abundance pattern.
 
@@ -107,6 +111,8 @@ class Abundances:
                 The depletion pattern
             depletion_model (class):
                 An instance of a synthesizer.depletion_models class.
+            depletion_scale (float):
+                A scaling factor to apply to the depletion pattern.
         """
         # Raise an exception if oxygen_to_hydrogen is used (not yet fully
         # implemented)
@@ -157,6 +163,7 @@ class Abundances:
         # depletion on to dust
         self.depletion_model = depletion_model
         self.depletion_pattern = None
+        self.depletion_scale = None
 
         # If a depletion pattern is provided use this directly.
         if depletion_pattern:
@@ -164,6 +171,24 @@ class Abundances:
 
         # Otherwise, if a depletion_model is provided...
         elif depletion_model:
+            if isinstance(depletion_model, str):
+                if depletion_model in depletion_models.available_patterns:
+                    self.depletion_model = getattr(
+                        depletion_models, depletion_model
+                    )
+                else:
+                    raise exceptions.UnrecognisedOption(
+                        "Depletion model not recognised!"
+                    )
+
+            # Check if self.depletion_model is instantiated and
+            # if not initialise class
+            if isinstance(self.depletion_model, type):
+                self.depletion_model = self.depletion_model()
+
+            # If a depletion scale is provided apply this to the depletion
+            if depletion_scale:
+                self.depletion_scale = depletion_scale
             self.depletion_pattern = self.depletion_model.depletion
 
         # If abundance pattern is provided as a string use this to extract the
