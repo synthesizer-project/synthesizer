3f213f70d8eb8e0f81bc7dfb5b4f1c81fc37920b
wjr21 <wjr21@sussex.ac.uk>
2026-04-02 00:58:09 +0100

    Add reductions extension module

--- src/synthesizer/extensions/reductions.h
+++ src/synthesizer/extensions/reductions.h
@@ -1,5 +1,10 @@
 #pragma once
 
+struct _object;
+typedef _object PyObject;
+
 // Reduction prototypes
 void reduce_spectra(double *spectra, double *part_spectra, int nlam, int npart,
                     int nthreads);
+
+PyObject *reduce_particle_spectra(PyObject *self, PyObject *args);
