88e2dfebb1df69ce79fcc1098ab59ec5ebed360e
wjr21 <wjr21@sussex.ac.uk>
2026-04-09 00:12:04 +0100

    Make sure we don't modify filter collections in place!

--- src/synthesizer/instruments/instrument_collection.py
+++ src/synthesizer/instruments/instrument_collection.py
@@ -37,6 +37,8 @@ Example usage:
 
 """
 
+from copy import deepcopy
+
 import h5py
 
 from synthesizer import exceptions
@@ -156,7 +158,7 @@ class InstrumentCollection:
             # Add the filters to the collection
             if instrument.can_do_photometry:
                 if self.all_filters is None:
-                    self.all_filters = instrument.filters
+                    self.all_filters = deepcopy(instrument.filters)
                 else:
                     self.all_filters += instrument.filters
 
