40d07d9c8d965bfeee700d07a25c6bf9423a99d7
Thomas Harvey <tharvey303@gmail.com>
2026-05-07 20:46:04 +0100

    Add tests and switch to inplace conversion

--- src/synthesizer/photometry.py
+++ src/synthesizer/photometry.py
@@ -14,7 +14,7 @@ import numpy as np
 from unyt import Hz, cm, erg, s
 
 from synthesizer import exceptions
-from synthesizer.units import Quantity, accepts
+from synthesizer.units import Quantity, accepts, unyt_to_ndview
 from synthesizer.utils.operation_timers import timed
 
 
@@ -84,12 +84,12 @@ class PhotometryCollection:
         is_flux = photometry.units.same_dimensions_as(fnu_unit)
 
         if is_flux:
-            self._photometry_data = photometry.to_value(fnu_unit)
+            self._photometry_data = unyt_to_ndview(photometry, fnu_unit)
             self.photo_fnu = self._photometry_data
             self.photo_lnu = None
         else:
             lnu_unit = self.__class__.__dict__["photo_lnu"].unit
-            self._photometry_data = photometry.to_value(lnu_unit)
+            self._photometry_data = unyt_to_ndview(photometry, lnu_unit)
             self.photo_lnu = self._photometry_data
             self.photo_fnu = None
 
