56d8e91c8203953b8ff4f7545692c288a493032b
wjr21 <wjr21@sussex.ac.uk>
2026-05-26 18:02:51 +0100

    Fuixing cyclic import

--- src/synthesizer/instruments/premade.py
+++ src/synthesizer/instruments/premade.py
@@ -62,8 +62,9 @@ import numpy as np
 from unyt import angstrom, arcsecond
 
 from synthesizer import INSTRUMENT_CACHE_DIR, exceptions
-from synthesizer.instruments import Instrument, InstrumentCollection
+from synthesizer.instruments import Instrument
 from synthesizer.instruments.filters import Filter, FilterCollection
+from synthesizer.instruments.instrument_collection import InstrumentCollection
 
 __all__ = [
     "JWSTNIRCamWide",
